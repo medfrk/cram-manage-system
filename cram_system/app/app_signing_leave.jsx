c3dff44d1d9da3b9d32fd4b1c1d9191390571655
wtlin1228 <wtlin0711@gmail.com>
2017-05-15 12:31:31 +0800

    remove unused files

--- cram_system/app/app_signing_leave.jsx
+++
@@ -1,8 +0,0 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var SigningLeave = require('SigningLeave');
-
-ReactDOM.render(
-  <SigningLeave />,
-  document.getElementById('app')
-);
